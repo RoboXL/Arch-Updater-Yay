7245198815e1e642bb711821790b7470ac2e0672
RoboXL <105053104+RoboXL@users.noreply.github.com>
2022-07-02 09:19:42 -0400

    Create Updater-yay-Base.sh
    Added a Base so people can change and see it's OG version

---
+++ Updater-yay-Base.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "########################################################################
+#         Welcome To Updater! Updating now..                           #
+########################################################################
+    "
+echo | yay -Syu 
+echo "########################################################################
+#        All Done! Whenever you are ready reboot the system            #
+########################################################################"
+echo "Note: Power users know if they have to reboot.
+If you are not a power user then you might wanna reboot"
+
+echo "Do you want to reboot? (Type 1 or 2)"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) make reboot; reboot;;
+        No ) echo "Ok Then you may close the window now or if you executed this 
+        using a command press ctrl+c";;
+    esac
+done
